0abbe69bd2cf01463961db3ab2839f2d610a9a7e
Kesa <chjo14@hotmail.com>
2023-12-03 02:17:40 -0500

    Add 2023-03 Part 1

--- Aoc2023/Day02.cs
+++ Aoc2023/Day02.cs
@@ -1,4 +1,5 @@
 ﻿using Kesa.AdventOfCode.Common;
+using System.Runtime.InteropServices;
 using System.Text.RegularExpressions;
 
 namespace Kesa.AdventOfCode.Aoc2023
@@ -20,7 +21,7 @@ namespace Kesa.AdventOfCode.Aoc2023
                     var gameId = gameMatch.GetGroup<int>("id");
                     var valid = true;
 
-                    foreach (Match match in itemMatches)
+                    foreach (var match in (IEnumerable<Match>)itemMatches)
                     {
                         var colorName = match.GetGroup("color");
                         var colorCount = match.GetGroup<int>("count");
@@ -38,6 +39,42 @@ namespace Kesa.AdventOfCode.Aoc2023
                     {
                         answer += gameId;
                     }
+                }
+            }
+
+            return answer.ToString();
+        }
+    }
+
+    internal class Day02_Part2 : IAocRunner
+    {
+        public static string Run(string input)
+        {
+            var gameRegex = new Regex(@"Game (?<id>\d+)");
+            var itemRegex = new Regex(@"(?:(?<count>\d+) (?<color>\w+))");
+            var colors = new Dictionary<string, int>();
+            var answer = 0;
+
+            foreach (var item in input.Lines())
+            {
+                colors.Clear();
+
+                if (true
+                    && gameRegex.Match(item) is { Success: true } gameMatch
+                    && itemRegex.Matches(item) is { Count: > 0 } itemMatches)
+                {
+                    foreach (var match in (IEnumerable<Match>)itemMatches)
+                    {
+                        var colorName = match.GetGroup("color");
+                        var colorCount = match.GetGroup<int>("count");
+
+                        ref var existingCount = ref CollectionsMarshal.GetValueRefOrAddDefault(colors, colorName, out var existed);
+
+                        existingCount = Math.Max(existingCount, colorCount);
+                    }
+
+                    var power = colors.Aggregate(1, (a, b) => a * b.Value);
+                    answer += power;
                 }
             }
 
---
+++ Aoc2023/Day03.cs
@@ -0,0 +1,105 @@
+﻿using Kesa.AdventOfCode.Common;
+using System.Text;
+
+namespace Kesa.AdventOfCode.Aoc2023
+{
+    internal class Day03 : IAocRunner
+    {
+        public static string Run(string input)
+        {
+            var lines = input.Lines().ToArray()!;
+            var number = new StringBuilder();
+            var isPartNumber = false;
+            var answer = 0;
+
+            for (int row = 0; row < lines.Length; row++)
+            {
+                var line = lines[row];
+
+                Reset();
+
+                for (int column = 0; column < line.Length + 1; column++)
+                {
+                    if (column == line.Length)
+                    {
+                        Check();
+                        continue;
+                    }
+
+                    var character = line[column];
+
+                    if (char.IsDigit(character))
+                    {
+                        number.Append(character);
+
+                        if (HasSymbolAround(row, column))
+                        {
+                            isPartNumber = true;
+                        }
+                    }
+                    else
+                    {
+                        Check();
+                    }
+                }
+            }
+
+            return answer.ToString();
+
+            void Check()
+            {
+                if (number.Length > 0)
+                {
+                    if (isPartNumber)
+                    {
+                        answer += int.Parse(number.ToString());
+                    }
+
+                    Reset();
+                }
+            }
+
+            void Reset()
+            {
+                isPartNumber = false;
+                number.Clear();
+            }
+
+            bool HasSymbolAround(int row, int column)
+            {
+                return false
+                    || IsPositionSymbol(row - 1, column - 1)
+                    || IsPositionSymbol(row - 1, column)
+                    || IsPositionSymbol(row - 1, column + 1)
+
+                    || IsPositionSymbol(row, column - 1)
+                    || IsPositionSymbol(row, column + 1)
+
+                    || IsPositionSymbol(row + 1, column - 1)
+                    || IsPositionSymbol(row + 1, column)
+                    || IsPositionSymbol(row + 1, column + 1);
+            }
+
+            bool IsPositionSymbol(int row, int column) => TryGetCharacter(row, column, out var character) && IsSymbol(character);
+
+            bool IsSymbol(char character) => !char.IsDigit(character) && character is not '.';
+
+            bool TryGetCharacter(int row, int column, out char character)
+            {
+                if (row >= 0 && row < lines!.Length)
+                {
+                    var line = lines[row];
+
+                    if (column >= 0 && column < line.Length)
+                    {
+                        character = line[column];
+                        return true;
+                    }
+                }
+
+                character = default;
+                return false;
+            }
+        }
+    }
+}
